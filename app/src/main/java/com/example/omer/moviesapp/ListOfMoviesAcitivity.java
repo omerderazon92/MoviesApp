44e24bf1d17b3ee3f9d2824de9c6372a978dd537
omer.derazon <omerderazon92@gmail.com>
2018-04-11 22:33:44 +0300

    handled some android studio warnnings

--- app/src/main/java/com/example/omer/moviesapp/ListOfMoviesAcitivity.java
+++ app/src/main/java/com/example/omer/moviesapp/ListOfMoviesAcitivity.java
@@ -8,12 +8,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
-
-/**
- * Created by Omer on 10/04/2018.
- */
 
 public class ListOfMoviesAcitivity extends AppCompatActivity {
 
@@ -27,11 +24,12 @@ public class ListOfMoviesAcitivity extends AppCompatActivity {
         setContentView(R.layout.list_of_movies_activity_layout);
         Intent intent = getIntent();
 
-        mRecyclerView = (RecyclerView) findViewById(R.id.list_of_movies_recycler_view);
+        mRecyclerView = findViewById(R.id.list_of_movies_recycler_view);
         mLayoutManager = new LinearLayoutManager(this);
         mRecyclerView.setLayoutManager(mLayoutManager);
         mRecyclerView.setHasFixedSize(true);
-        mAdapter = new MoviesAdapter(ListOfMoviesAcitivity.this, (List<Movie>) intent.getSerializableExtra("list"), mRecyclerView);
+        final List<Movie> list = (List<Movie>) intent.getSerializableExtra("list");
+        mAdapter = new MoviesAdapter(ListOfMoviesAcitivity.this,  list, mRecyclerView);
         mRecyclerView.setAdapter(mAdapter);
 
     }
